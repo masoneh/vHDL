e754d45082ed8f62bc9c7b7affdf2039db8e18e8
groteab25 <116527193+groteab25@users.noreply.github.com>
2023-03-24 12:05:34 -0600

    Update Window.v

--- Window.v
+++ Window.v
@@ -4,18 +4,18 @@ module Window(input_function, new_function, coefficients);
 
     parameter size = 8;
     parameter arg = `M_TWO_PI / size;
-    input [size*7-1:0] input_function;
-    output [size*7-1:0] new_function; 
-    reg [size*7-1:0] new_function;
-	 input [size*7-1:0] coefficients;
-	 integer j;
+    input [size*8-1:0] input_function;
+    output [size*8-1:0] new_function; 
+    reg [size*8-1:0] new_function;
+    input [size*8-1:0] coefficients;
+    integer j;
 
 	 generate 
     always@(input_function)
         begin 
             // apply hamming window to input  
             for(j=0; j<size; j=j+1) begin
-                new_function[j*7 +: 7] = coefficients[j*7 +: 7] * input_function[j*7 +: 7];  
+		 new_function[j*8 +: 8] = coefficients[j*8 +: 8] * input_function[j*8 +: 8];  
             end 
         end
 	 endgenerate
