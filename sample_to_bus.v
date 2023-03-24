3755a683e1ecbe00fa0275bd3c9f8d97d02edd5c
masoneh <113065016+masoneh@users.noreply.github.com>
2023-03-24 12:35:28 -0600

    Update sample_to_bus.v

--- sample_to_bus.v
+++ sample_to_bus.v
@@ -1,21 +1,36 @@
 module sample_to_bus(
 input 50mHzclk; //FPGA 50 mHz clk 
-input wire [7:0] sample; // 8-bit sample input from ADC (pico)
+input bit0, bit1, bit2, bit3, bit4, bit5, bit6, bit7; // 1 bit inputs 
 output reg [63:0] out; // 64-bit bus comprised of 8 samples
 );
 
 
-reg [7:0] samplebuf [0:7]; // to store 8 samples
+reg [7:0] samplebuf; // to store 8 samples
 reg [2:0] count; // count to track num of samples entered
 
 
 always@(posedge 50mHzclk) begin
-    samplebuf[count] <= sample; // store sample into bus
-    count <= count + 1; // increase counter 
+   samplebuf[0] <= bit0;
+   samplebuf[1] <= bit1;
+   samplebuf[2] <= bit2;
+   samplebuf[3] <= bit3;
+   samplebuf[4] <= bit4;
+   samplebuf[5] <= bit5;
+   samplebuf[6] <= bit6;
+   samplebuf[7] <= bit7;
 
-    if (count == 7) begin // when count = 7, all 8 samples have been receieved 
-        out <= {samplebuf[7], samplebuf[6], samplebuf[5], samplebuf[4], samplebuf[3], samplebuf[2], samplebuf[1], samplebuf[0]}; // set each 8 bit sample to allocated 8 bit spot on output bus
-        count <= 0; // reset counter
-    end
+    case (count) 
+        3'b000: begin out[7:0] <= samplebuf; count <= count + 1; end
+        3'b001: begin out[15:8] <= samplebuf; count <= count + 1; end
+        3'b010: begin out[23:16] <= samplebuf; count <= count + 1; end
+        3'b011: begin out[31:24] <= samplebuf; count <= count + 1; end
+        3'b100: begin out[39:32] <= samplebuf; count <= count + 1; end
+        3'b101: begin out[47:40] <= samplebuf; count <= count + 1; end
+        3'b110: begin out[55:48] <= samplebuf; count <= count + 1; end
+        3'b111: begin out[63:56] <= samplebuf; count <= count + 1; end
+        defaut: count <= 0;
+    endcase
+    
+ 
 end
 endmodule 
