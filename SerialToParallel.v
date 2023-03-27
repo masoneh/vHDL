1a8647831f4cce0bdb0e767c0eff535a7ffbe2ad
groteab25 <116527193+groteab25@users.noreply.github.com>
2023-03-27 13:53:36 -0600

    Update SerialToParallel.v

--- SerialToParallel.v
+++ SerialToParallel.v
@@ -1,31 +1,32 @@
 module SerialToParallel(D, clk, reset, Q0, Q1, Q2, Q3, Q4, Q5, Q6, Q7);
-	input D; 
+	input [63:0] D; 
 	input clk; //rising edge D flip flop
 	input reset;
-	output reg Q0, Q1, Q2, Q3, Q4, Q5, Q6, Q7;
+	output reg [7:0] Q0, Q1, Q2, Q3, Q4, Q5, Q6, Q7;
 	
 	always @(posedge clk) 
 	begin
 		if(reset==1'b1) begin 
-			Q0<=1'b0;
-			Q1<=1'b0;
-			Q2<=1'b0;
-			Q3<=1'b0;
-			Q4<=1'b0;
-			Q5<=1'b0;
-			Q6<=1'b0;
-			Q7<=1'b0;
+			Q0<=8'h00;
+			Q1<=8'h00;
+			Q2<=8'h00;
+			Q3<=8'h00;
+			Q4<=8'h00;
+			Q5<=8'h00;
+			Q6<=8'h00;
+			Q7<=8'h00;
 		end 
 		else begin 
-			Q0 <= D;
-			Q1 <= Q0;
-			Q2 <= Q1;
-			Q3 <= Q2;
-			Q4 <= Q3;
-			Q5 <= Q4;
-			Q6 <= Q5;
-			Q7 <= Q6;
+			Q0 <= D[7:0];
+			Q1 <= Q0[7:0];
+			Q2 <= Q1[7:0];
+			Q3 <= Q2[7:0];
+			Q4 <= Q3[7:0];
+			Q5 <= Q4[7:0];
+			Q6 <= Q5[7:0];
+			Q7 <= Q6[7:0];
 		end
 	end
 endmodule 
+	 
 	
