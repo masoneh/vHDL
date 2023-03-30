97704c28b320634017bac7021254c736e8efbe17
TheaZhuu <101103060+TheaZhuu@users.noreply.github.com>
2023-03-29 20:36:27 -0600

    Add files via upload

---
+++ S2P/DFF_colck_enable.v
@@ -0,0 +1,40 @@
+///////////////
+////Thea Zhu///
+///////////////
+
+
+//DFF with clock enable 
+
+
+module DFF_colck_enable(d,clk,en,q);
+	/* 
+	d determines the next value of the output. the output doesn't change 
+	unitl the next rising edge of the clock. 
+	
+	clk determines when the output will change. the output changes when a rising edge of
+	the clk is detected.
+	
+	q is output and stored value
+	
+	
+	*/
+
+	input d,clk,en;
+
+	output reg q;
+	
+	initial begin
+		q = 0;
+	end
+	
+	always @(posedge clk) begin
+		 if (en) begin
+			  q <=d;
+		 end
+		 else begin
+			  q <=q;
+		 end
+	end
+endmodule
+		
+	
