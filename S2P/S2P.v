e87cedd50f17681de3eb846637fe1e0362e27ba0
TheaZhuu <101103060+TheaZhuu@users.noreply.github.com>
2023-03-31 12:57:02 -0600

    Update S2P.v

--- S2P/S2P.v
+++ S2P/S2P.v
@@ -2,7 +2,7 @@
 ////Thea Zhu///
 ///////////////
 
-//`include "DFF_colck_enable.v"
+//`include "DFF_colck_enable_16.v"
 
 //Serial to Parallel Conversion
 
@@ -17,15 +17,18 @@ module S2P(a,clk,en,y1,y2,y3,y4,y5,y6,y7,y8);
 			//y1 is MSB, y8 is LSB
 			output [(2**N)-1:0] y1,y2,y3,y4,y5,y6,y7,y8; 
 			
-			//DFF_colck_enable(d,clk,en,q);
+			//DFF_clock_enable_16(a,clk,en,y)
 			
-			DFF_colck_enable_16 d1(a, clk, en, y1);
-			DFF_colck_enable_16 d2(y1, clk, en, y2);
-			DFF_colck_enable_16 d3(y2, clk, en, y3);
-			DFF_colck_enable_16 d4(y3, clk, en, y4);
-			DFF_colck_enable_16 d5(y4, clk, en, y5);
-			DFF_colck_enable_16 d6(y5, clk, en, y6);
-			DFF_colck_enable_16 d7(y6, clk, en, y7);
-			DFF_colck_enable_16 d8(y7, clk, en, y8);
+			DFF_clock_enable_16 d1 (a, clk, en, y1);
+			DFF_clock_enable_16 d2 (y1, clk, en, y2);
+			DFF_clock_enable_16 d3 (y2, clk, en, y3);
+			DFF_clock_enable_16 d4 (y3, clk, en, y4);
+			DFF_clock_enable_16 d5 (y4, clk, en, y5);
+			DFF_clock_enable_16 d6 (y5, clk, en, y6);
+			DFF_clock_enable_16 d7 (y6, clk, en, y7);
+			DFF_clock_enable_16 d8 (y7, clk, en, y8);
 	
 endmodule
+
+
+
