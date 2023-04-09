fe85646e0b4cd93a6c2c33fc7b056f60206cf3c3
TheaZhuu <101103060+TheaZhuu@users.noreply.github.com>
2023-04-09 10:52:50 -0600

    Add files via upload

--- FPGA/FPGA.v
+++ FPGA/FPGA.v
@@ -28,7 +28,7 @@ module FPGA (bit0, bit1, bit2, bit3, bit4, bit5, bit6, bit7,clk,rst, output_re, 
 		eight_pt_fft fft(NewQ0, NewQ1, NewQ2, NewQ3, NewQ4, NewQ5, NewQ6, NewQ7,clk,rst,
                     y0,yr1,yi1,yr2,yi2,yr3,yi3,y4,yr5,yi5,yr6,yi6,yr7,yi7);
 		
-		fft_output(y0,yr1,yi1,yr2,yi2,yr3,yi3,y4,yr5,yi5,yr6,yi6,yr7,yi7,
+		fft_output fo(y0,yr1,yi1,yr2,yi2,yr3,yi3,y4,yr5,yi5,yr6,yi6,yr7,yi7,
 					clk, output_re, output_im, index);
 endmodule
 
